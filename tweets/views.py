f607e4a423f18f9b3ad6b6c3dbbda5a087f17f97
argonsys <kenon12552@icloud.com>
2023-12-20 17:56:43 +0900

    一覧機能実装

--- tweets/views.py
+++ tweets/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 
+from tweets.models import Tweet
 
 def tweets_index(request):
-    return render(request, "tweets_index.html")
+    tweets = Tweet.objects.all()
+    return render(request, "tweets_index.html", {"tweets": tweets})
