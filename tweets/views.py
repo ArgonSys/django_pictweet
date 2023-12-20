5f2be0343618eacc7e7bd0bd19adc2334e1b0ccd
argonsys <kenon12552@icloud.com>
2023-12-20 17:47:11 +0900

    tweets_indexのビューとテンプレート作成

--- tweets/views.py
+++ tweets/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Create your views here.
+
+def tweets_index(request):
+    return render(request, "tweets_index.html")
