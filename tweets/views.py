ea5bf0ce33c2d5bbcc10721ef46f3b4b073620b9
argonsys <kenon12552@icloud.com>
2023-12-20 18:48:03 +0900

    Noneなimageを""にする

--- tweets/views.py
+++ tweets/views.py
@@ -3,5 +3,11 @@ from django.shortcuts import render, redirect
 from tweets.models import Tweet
 
 def tweets_index(request):
-    tweets = Tweet.objects.all()
-    return render(request, "tweets_index.html", {"tweets": tweets})
+    context = {"tweets": __sanitize_image(Tweet.objects.all())}
+    return render(request, "tweets_index.html", context)
+
+
+def __sanitize_image(tweets):
+    for tweet in tweets:
+        tweet.image = tweet.image or ""
+    return tweets
