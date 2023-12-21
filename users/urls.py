c1eca3de7e13958ad1ecb13050eb350e33654735
argonsys <kenon12552@icloud.com>
2023-12-21 18:02:32 +0900

    UsersViewの作成

--- users/urls.py
+++ users/urls.py
@@ -1,23 +1,12 @@
 from django.urls import path
-from django.contrib.auth.views import LoginView, LogoutView
-from django.views.generic import CreateView
+from django.contrib.auth.views import LogoutView
+from .views import SignupView, LoginView
 
-from .forms import LoginForm, SignupForm
 
 app_name = "users"
 
 urlpatterns = [
-    path("signup/", CreateView.as_view(
-        template_name="users/signup.html",
-        form_class=SignupForm,
-        success_url="/",
-    ), name="signup"),
-    path("login/", LoginView.as_view(
-        redirect_authenticated_user=True,
-        template_name="users/login.html",
-        form_class=LoginForm,
-        success_url="/",
-    ), name="login"),
+    path("signup/", SignupView.as_view(), name="signup"),
+    path("login/", LoginView.as_view(), name="login"),
     path("logout/", LogoutView.as_view(), name="logout"),
-
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.contrib.auth.views import LoginView as LoginBaseView
+from django.views.generic import CreateView
 
-# Create your views here.
+from .forms import LoginForm, SignupForm
+
+
+class SignupView(CreateView):
+    template_name = "users/signup.html"
+    form_class = SignupForm
+    success_url = "/"
+
+class LoginView(LoginBaseView):
+    redirect_authenticated_user = True
+    template_name = "users/login.html"
+    form_class = LoginForm
+    success_url = "/"
