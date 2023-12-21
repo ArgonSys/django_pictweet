fede53dbd1c7a4460f1dab1194bda3b964a38864
argonsys <kenon12552@icloud.com>
2023-12-21 14:46:23 +0900

    仮のログインページ実装

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-
+from django.contrib.auth.views import LoginView, LogoutView
 
 
 app_name = "users"
 
 urlpatterns = [
-
+    path("login", LoginView.as_view(
+        redirect_authenticated_user=True,
+        template_name="users/login.html"
+    ), name="login"),
 ]
