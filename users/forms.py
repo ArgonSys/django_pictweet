148d08e6a9e16bfc358335bc2d3191739d66bb13
argonsys <kenon12552@icloud.com>
2023-12-21 17:49:05 +0900

    SignUpフォーム作成

--- users/forms.py
+++ users/forms.py
@@ -1,8 +1,33 @@
-from django.contrib.auth import forms
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django import forms
 
-class LoginForm(forms.AuthenticationForm):
+from users.models import User
+
+
+class LoginForm(AuthenticationForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.label_suffix = ""
         for field in self.fields.values():
             pass
+
+    class Meta:
+        model = User
+        fields = ("email", "password")
+
+class SignupForm(UserCreationForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.label_suffix = ""
+
+    class Meta:
+        model = User
+        fields = ("username", "email")
+        widgets = {
+            "username": forms.TextInput(attrs={
+
+            }),
+            "email": forms.EmailInput(attrs={
+
+            }),
+        }
