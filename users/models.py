63f01f4da3724df52ee987ead3beb7c4605c4a18
argonsys <kenon12552@icloud.com>
2023-12-21 15:55:54 +0900

    カスタムユーザーモデルの編集

--- users/models.py
+++ users/models.py
@@ -1,7 +1,44 @@
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin, UserManager
+from django.contrib.auth.validators import ASCIIUsernameValidator
+from django.utils import timezone
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+
+class User(AbstractBaseUser, PermissionsMixin):
+    username_validator = ASCIIUsernameValidator()
+    username = models.CharField(
+        "ユーザー名",
+        max_length=150,
+        unique=False,
+        validators=[username_validator],
+    )
+
+    email = models.EmailField(
+        "メールアドレス",
+        unique=True,
+        error_messages={
+            "unique": "Email address has already token",
+        },
+    )
+
+    is_admin = models.BooleanField(default=False)
+    is_staff = models.BooleanField(
+        "staff status",
+        default=False,
+    )
+    is_active = models.BooleanField(
+        "active",
+        default=True,
+    )
+    date_joined = models.DateTimeField("date joined", default=timezone.now)
 
 
-class User(AbstractUser):
+    objects = UserManager()
+
+
+    EMAIL_FIELD = "email"
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
     class Meta:
-        db_table = 'auth_user'
+        db_table = 'users'
