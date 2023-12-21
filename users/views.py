24aaedaed0848fbd0725ec3392af8da3a90168f0
argonsys <kenon12552@icloud.com>
2023-12-21 18:06:15 +0900

    新規登録後ログイン’する機能を実装

--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.views import LoginView as LoginBaseView
 from django.views.generic import CreateView
+from django.contrib.auth import login, authenticate
 
 from .forms import LoginForm, SignupForm
 
@@ -9,6 +10,15 @@ class SignupView(CreateView):
     template_name = "users/signup.html"
     form_class = SignupForm
     success_url = "/"
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        email = form.cleaned_data.get("email")
+        password = form.cleaned_data.get("password1")
+        user = authenticate(email=email, password=password)
+        login(self.request, user)
+        return response
+
 
 class LoginView(LoginBaseView):
     redirect_authenticated_user = True
