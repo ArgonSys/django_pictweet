dae8b14c22a9969dd1a476f91a0bb97450922229
argonsys <kenon12552@icloud.com>
2023-12-21 17:49:59 +0900

    signupのルーティング

--- users/urls.py
+++ users/urls.py
@@ -1,15 +1,23 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
-from .forms import LoginForm
+from django.views.generic import CreateView
+
+from .forms import LoginForm, SignupForm
 
 app_name = "users"
 
 urlpatterns = [
-    path("login", LoginView.as_view(
+    path("signup/", CreateView.as_view(
+        template_name="users/signup.html",
+        form_class=SignupForm,
+        success_url="/",
+    ), name="signup"),
+    path("login/", LoginView.as_view(
         redirect_authenticated_user=True,
         template_name="users/login.html",
         form_class=LoginForm,
+        success_url="/",
     ), name="login"),
-    path("logout", LogoutView.as_view(), name="logout"),
+    path("logout/", LogoutView.as_view(), name="logout"),
 
 ]
