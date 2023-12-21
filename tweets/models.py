5ee541bbef7b6fe647e1ac0a484ff72919dc53fe
argonsys <kenon12552@icloud.com>
2023-12-21 21:38:00 +0900

    tweetsにcreated_byを追加

--- tweets/models.py
+++ tweets/models.py
@@ -1,11 +1,20 @@
 from django.db import models
+from django.conf import settings
+from users.models import User
 
 class Tweet(models.Model):
     name = models.CharField("名前", max_length=128)
     text = models.TextField("本文")
     image = models.CharField("画像URL", max_length=512, blank=True, null=True)
+    created_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        verbose_name="投稿者",
+        on_delete=models.CASCADE,
+        default=1
+    )
     created_at = models.DateTimeField("投稿日時", auto_now_add=True)
     updated_at = models.DateTimeField("投稿日時", auto_now=True)
+
 
     class Meta:
         db_table = "tweets"
