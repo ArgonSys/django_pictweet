329b38e23e49772dc20da0df1b9d719d96bd60f6
argonsys <kenon12552@icloud.com>
2023-12-21 21:38:35 +0900

    created_byのdefault削除

--- tweets/models.py
+++ tweets/models.py
@@ -9,8 +9,7 @@ class Tweet(models.Model):
     created_by = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         verbose_name="投稿者",
-        on_delete=models.CASCADE,
-        default=1
+        on_delete=models.CASCADE
     )
     created_at = models.DateTimeField("投稿日時", auto_now_add=True)
     updated_at = models.DateTimeField("投稿日時", auto_now=True)
