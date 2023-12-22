d79e7f803fc9444c65ae89131921b1c7ee49455a
argonsys <kenon12552@icloud.com>
2023-12-22 15:47:06 +0900

    Commentモデルの作成

--- comments/models.py
+++ comments/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from users.models import User
+from tweets.models import Tweet
 
-# Create your models here.
+
+class Comment(models.Model):
+    text = models.CharField(max_length=128)
+    created_by = models.ForeignKey(
+        User,
+        verbose_name="投稿者",
+        on_delete=models.CASCADE
+    )
+    created_in = models.ForeignKey(
+        Tweet,
+        verbose_name="ツイート",
+        on_delete=models.CASCADE
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = "comments"
+
+    def __str__(self):
+        return f"{self.pk} {self.text}"
