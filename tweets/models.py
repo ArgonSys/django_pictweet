7adf6cfd23de36dd53b83da22e2ffccd6fa6ffc6
argonsys <kenon12552@icloud.com>
2023-12-22 15:29:39 +0900

    降順になるようにTweetManagerを作成

--- tweets/models.py
+++ tweets/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 from django.conf import settings
 
+
+class TweetManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().order_by("-created_at")
+
+
 class Tweet(models.Model):
     text = models.TextField("本文")
     image = models.CharField("画像URL", max_length=512, blank=True, null=True)
@@ -12,6 +18,7 @@ class Tweet(models.Model):
     created_at = models.DateTimeField("投稿日時", auto_now_add=True)
     updated_at = models.DateTimeField("投稿日時", auto_now=True)
 
+    objects = TweetManager()
 
     class Meta:
         db_table = "tweets"
